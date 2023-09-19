944316db12f1381b3f237b8c75c26f0bf67e5357
Dmitry Dazko <dmitrydatsko.of@gmail.com>
2023-09-19 18:24:16 +0300

    tag search id

--- CircleCI/CircleCI.Entities/DTOs/Responses/GetTagsResponse.cs
+++ CircleCI/CircleCI.Entities/DTOs/Responses/GetTagsResponse.cs
@@ -2,6 +2,7 @@ namespace CircleCI.Entities.DTOs.Responses;
 
 public class GetTagsResponse
 {
+    public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string ImageUrl { get; set; } = string.Empty;
 }
